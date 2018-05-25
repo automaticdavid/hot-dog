203e30f09d0bb34993009495fdf05559ca205036
zen <zen@localhost.localdomain>
2018-05-25 14:12:04 -0400

    is it hot dog, or not hot dog ?

---
+++ app/app.py
@@ -0,0 +1,47 @@
+import os
+import socket
+from flask import Flask, render_template, url_for
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
+from lib import NotSanta
+
+dir_results = os.getcwd() + '/static/'
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'I have a dream'
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads/'
+app.config['RESULTS'] = dir_results
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+app.config['MODEL'] = "model/santa_not_santa.model"
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)  # set maximum file size, default is 16MB
+
+curr_host = socket.gethostname() 
+curr_ip = socket.gethostbyname(socket.gethostname())
+
+class UploadForm(FlaskForm):
+    photo = FileField(validators=[FileAllowed(photos, u'Image only!'), FileRequired(u'File was empty!')])
+    submit = SubmitField(u'Upload')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = photos.save(form.photo.data)
+        result = NotSanta().classify(app.config['MODEL'], 'uploads/' + filename)
+        result_name = os.path.basename(result)
+    else:
+        result_name = None
+    print(">>>", result_name)
+
+    return render_template('index.html', form=form, file=result_name, host=curr_host, ip=curr_ip)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ app/lib.py
@@ -0,0 +1,55 @@
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+from keras import backend as K
+import numpy as np
+import imutils
+import cv2
+import tempfile
+
+class NotSanta:
+
+    def classify(self, model_name, image_path):
+        
+        # load the trained convolutional neural network
+        model = load_model(model_name)
+
+        # load the image
+        image = cv2.imread(image_path)
+        orig = image.copy()
+
+        # pre-process the image for classification
+        image = cv2.resize(image, (28, 28))
+        image = image.astype("float") / 255.0
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+
+        # classify the input image
+        (not_santa, santa) = model.predict(image)[0]
+
+        # build the label
+
+        if santa > not_santa:
+            label = "Santa"
+            proba = santa
+        else: 
+            label = "Not Santa"
+            proba = not_santa
+
+        label = "{}: {:.2f}%".format(label, proba * 100)
+
+        # draw the label on the image
+        output = imutils.resize(orig, width=400)
+
+        cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,
+	    1, (0, 255, 0), 3)
+
+        f = tempfile.NamedTemporaryFile(mode='w+b', delete=False, dir='static', suffix='.jpg')
+        cv2.imwrite( f.name, output)
+
+        del model
+        K.clear_session()
+
+        return(f.name)
+
+
+
