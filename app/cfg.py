1fdaace546bff83d517af0d0c7284c7768860992
root <root@localhost.localdomain>
2018-06-07 22:58:35 +0200

    added cfg.py

--- app/cfg.py
+++ app/cfg.py
@@ -1,11 +1,9 @@
 bind = "127.0.0.1:80"
 workers = 5
 threads = 5
-max-requests = 5
-preload = true
-daemon = true
-access-logfile = "/tmp/gunicorn.access.log"
-capture-output = true
-error-logfile = "/tmp/gunicorn.error.log"
+preload = True
+daemon = True
+accesslog = '/tmp/gunicorn.access.log'
+errorlog = '/tmp/gunicorn.error.log'
 
 
