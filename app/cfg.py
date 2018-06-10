bfbe52077792fdd46efbdeafd550e05af2d521bc
root <root@localhost.localdomain>
2018-06-10 21:07:43 +0200

    Added gunicorn raw_env

--- app/cfg.py
+++ app/cfg.py
@@ -1,3 +1,4 @@
+import os
 bind = "0.0.0.0:80"
 workers = 2
 threads = 2
@@ -7,11 +8,18 @@ errorlog = '/tmp/gunicorn.error.log'
 reload = True
 #enable_stdio_inheritance = True
 #loglevel = 'debug'
+
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+S3_KEY = os.environ.get("S3_KEY")
+S3_SECRET = os.environ.get("S3_SECRET")
+S3_LOCATION = 'http://{}.s3.amazonoaws.com/'.format(S3_BUCKET_NAME)
+
+
 raw_env =  [ 
-    S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
-    S3_KEY = os.environ.get("S3_KEY")
-    S3_SECRET = os.environ.get("S3_SECRET")
-    S3_LOCATION = 'http://{}.s3.amazonoaws.com/'.format(S3_BUCKET_NAME)
+    'S3_BUCKET_NAME =' + S3_BUCKET_NAME, 
+    'S3_KEY = ' + S3_KEY,
+    'S3_SECRET = ' + S3_SECRET,
+    'S3_LOCATION = ' + S3_LOCATION
     ]
 
 
